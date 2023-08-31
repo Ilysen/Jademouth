409855fff9aebe97bc39318e30bb9f8169e89299
Ava <ava.perhaps+github@pm.me>
2023-08-31 00:15:48 -0400

    tune Chaos Theory XP, rewrite Bright's dialogue

---
+++ Jademouth/Scripts/ConversationDelegates.cs
@@ -0,0 +1,13 @@
+﻿namespace XRL.World.Conversations
+{
+	[HasConversationDelegate]
+	public static class JademouthConversationDelegates
+	{
+		/// <summary>
+		/// Checks if the context target's current cell is illuminated with <b>normal light.</b> Night vision and the like won't apply.<br/>
+		/// Used in Bright's dialogue to show a separate intro for people who have a light source equipped, since she's sensitive to bright lights.
+		/// </summary>
+		[ConversationDelegate(Speaker = true)]
+		public static bool IfIlluminated(DelegateContext Context) => Context.Target.CurrentCell.GetLight() == XRL.World.LightLevel.Light;
+	}
+}
